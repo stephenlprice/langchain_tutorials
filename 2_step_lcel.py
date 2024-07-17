6ffd458da8624823b8088c9aac136dfe4c267250
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2024-07-17 08:58:09 -0600

    expanding on the first tutorial

--- 2_step_lcel.py
+++ 2_step_lcel.py
@@ -6,6 +6,7 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langserve import add_routes
 from dotenv import load_dotenv
+from langchain.chains import TransformChain
 
 load_dotenv()
 
@@ -22,19 +23,27 @@ model = ChatOpenAI()
 # 3. Create parser
 parser = StrOutputParser()
 
-# 4. Create chain
-chain = prompt_template | model | parser
+# 4. Create uppercase transform function
+def uppercase_transform(inputs: dict) -> dict:
+    return {"uppercase_text": inputs["text"].upper()}
 
+uppercase_chain = TransformChain(
+    input_variables=["text"],
+    output_variables=["uppercase_text"],
+    transform=uppercase_transform
+)
 
-# 4. App definition
+# 5. Create chain
+chain = prompt_template | model | parser | uppercase_chain
+
+# 6. App definition
 app = FastAPI(
   title="LangChain Server",
   version="1.0",
   description="A simple API server using LangChain's Runnable interfaces",
 )
 
-# 5. Adding chain route
-
+# 7. Adding chain route
 add_routes(
     app,
     chain,
